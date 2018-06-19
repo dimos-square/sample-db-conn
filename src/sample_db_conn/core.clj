318504bf6eda3268cd90702b9cbcde8ab603a618
dimos-square <dimosthenis@baresquare.com>
2018-06-19 17:27:22 +0300

    read env variable

--- src/sample_db_conn/core.clj
+++ src/sample_db_conn/core.clj
@@ -1,7 +1,11 @@
 (ns sample-db-conn.core
-  (:gen-class))
+  (:gen-class)
+  (:require [environ.core :as env]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn env-variable [var-k]
+  (env/env var-k))
